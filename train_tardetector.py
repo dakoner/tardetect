98c5d1d24f06137ed4b8d27d3cc712103b9fb5e2
dek <dakoner@gmail.com>
2022-02-20 17:50:52 -0800

    add labels file and training scripts

--- train_tardetector.py
+++ train_tardetector.py
@@ -19,7 +19,6 @@ from six import BytesIO
 from PIL import Image, ImageDraw, ImageFont
 import tensorflow as tf
 
-# from object_detection.utils import label_map_util
 from object_detection.utils import config_util
 from object_detection.utils import visualization_utils as viz_utils
 from object_detection.builders import model_builder
