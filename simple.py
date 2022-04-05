bc00a6b4c0583efcc03ba7eb3ed6c9dc7a681dd6
dek <dakoner@gmail.com>
2022-04-05 13:52:35 +0000

    fix multiclass drawing

--- simple.py
+++ simple.py
@@ -14,27 +14,27 @@ import time
 from object_detection.utils import config_util
 from object_detection.utils import visualization_utils as viz_utils
 from object_detection.builders import model_builder
-from classes import num_classes, category_index
+from classes import num_classes, category_index, id_to_name
 import cv2
 from util import load_image_into_numpy_array, plot_detections
 from model import build_model_and_restore_weights
 BASE=r'z:\src'
 
 
-detection_model = build_model_and_restore_weights()
+detection_model = build_model_and_restore_weights(num_classes)
 
 ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
 ckpt.restore("tardigrade-1-1")
 
 label_id_offset = 1
   
-fontScale = 1
+fontScale = 0.25
 color = (255, 0, 0)
 thickness = 2
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 #p = r"tardetect\tardigrade movies\outpy.2.mkv"
-p = r"microscope_ui\outpy.mkv"
+p = r"c:\users\dek\Desktop\tardigrade movies\outpy.4.mkv"
 
 r = cv2.VideoCapture(os.path.join(BASE, p))
 
@@ -48,15 +48,18 @@ while True:
     prediction_dict = detection_model.predict(preprocessed_image, shapes)
     detections = detection_model.postprocess(prediction_dict, shapes)
     scores = detections['detection_scores'][0].numpy()
-    if scores[0] > .2:
-        dbs = detections['detection_boxes'][0].numpy()
-        db = dbs[0]
-        s = frame.shape
-        pt1 = int(db[1]*s[1]), int(db[0]*s[0])
-        pt2 = int(db[3]*s[1]), int(db[2]*s[0])
-        cv2.putText(frame, "%5.2f" % scores[0], pt2, font, fontScale, color, thickness, cv2.LINE_AA)
-        cv2.rectangle(frame, pt1, pt2, (0, 128, 255), 1)
+    detection_boxes = detections['detection_boxes'][0].numpy()
+    detection_classes = detections['detection_classes'][0].numpy()
+    for i, score in enumerate(scores):
+        if score > .6:
+            db = detection_boxes[i]
+            s = frame.shape
+            pt1 = int(db[1]*s[1]), int(db[0]*s[0])
+            pt2 = int(db[3]*s[1]), int(db[2]*s[0])
+            label = detection_classes[i]
+            cv2.putText(frame, "%s %5.2f" % (id_to_name[int(label+1)], score), pt2, font, fontScale, color, thickness, cv2.LINE_AA)
+            cv2.rectangle(frame, pt1, pt2, (0, 128, 255), 1)
     cv2.imshow('im',frame)
     cv2.waitKey(1) 
     t1 = time.time()
-    #print(t1-t0)
+    print(t1-t0)
